76d38b02ed647d1e5a5d079fd0e0e2b68cd3b1a5
Ritvik Gupta <69435625+theseus-alt@users.noreply.github.com>
2022-01-05 19:06:45 +0530

    fixed typos in help.py

--- help.py
+++ help.py
@@ -1,8 +1,8 @@
 help={
-    "info":"Enter id of a doujin to get brief information about it.",
+    "info":"Enter id of a doujin to get brief information about it. \n eg. nh!info 117013",
     "cover":"Enter id of a doujin to get it's cover image. \n eg. nh!cover 117013",
     "random":"Get information about a random doujin.",
-    "search":"Seach the nhentai database using using a term followed by number of results you want. \n eg. nh! naruto 5 \n Note: Search queries with more than 5 doujins may take few seconds to process",
+    "search":"Seach the nhentai database using using a term followed by number of results you want. \n eg. nh!search naruto 5 \n Note: Search queries with more than 5 doujins may take few seconds to process",
     "pop":"Get trending doujins in the current time."
 }
 
