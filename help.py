cb251249a1df5e760c1787152d3f5c5dc82f8098
Ritvik Gupta <69435625+theseus-alt@users.noreply.github.com>
2022-01-05 18:50:50 +0530

    Update help.py

--- help.py
+++ help.py
@@ -1,5 +1,5 @@
 help={
-    "info":"Enter id of a doujin to get brief information about i.t",
+    "info":"Enter id of a doujin to get brief information about it.",
     "cover":"Enter id of a doujin to get it's cover image. \n eg. nh!cover 117013",
     "random":"Get information about a random doujin.",
     "search":"Seach the nhentai database using using a term followed by number of results you want. \n eg. nh! naruto 5 \n Note: Search queries with more than 5 doujins may take few seconds to process",
